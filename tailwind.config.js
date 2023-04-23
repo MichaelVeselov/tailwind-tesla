0e2360cda7b9da03dd5981b0e07e0b34525807b6
Michael Veselov <michael.veselov@gmail.com>
2023-04-23 18:29:28 +0300

    change project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./build/**/*.{html,js}'],
   theme: {
     screens: {
       tablet: '600px',
@@ -13,20 +13,20 @@ module.exports = {
         border: '0 2px 0 0 #181b21',
       },
       backgroundImage: (theme) => ({
-        'model-3': "url('../dist/images/model-3.jpeg')",
-        'mobile-model-3': "url('../dist/images/mobile-model-3.jpeg')",
-        'model-s': "url('../dist/images/model-s.jpeg')",
-        'mobile-model-s': "url('../dist/images/mobile-model-s.jpeg')",
-        'model-y': "url('../dist/images/model-y.jpeg')",
-        'mobile-model-y': "url('../dist/images/mobile-model-y.jpeg')",
-        'model-x': "url('../dist/images/model-x.jpeg')",
-        'mobile-model-x': "url('../dist/images/mobile-model-x.jpeg')",
-        'solar-panels': "url('../dist/images/solar-panels.jpeg')",
-        'mobile-solar-panels': "url('../dist/images/mobile-solar-panels.jpeg')",
-        'solar-roof': "url('../dist/images/solar-roof.jpeg')",
-        'mobile-solar-roof': "url('../dist/images/mobile-solar-roof.jpeg')",
-        accessories: "url('../dist/images/accessories.jpeg')",
-        'mobile-accessories': "url('../dist/images/mobile-accessories.jpeg')",
+        'model-3': "url('./images/model-3.jpeg')",
+        'mobile-model-3': "url('./images/mobile-model-3.jpeg')",
+        'model-s': "url('./images/model-s.jpeg')",
+        'mobile-model-s': "url('./images/mobile-model-s.jpeg')",
+        'model-y': "url('./images/model-y.jpeg')",
+        'mobile-model-y': "url('./images/mobile-model-y.jpeg')",
+        'model-x': "url('./images/model-x.jpeg')",
+        'mobile-model-x': "url('./images/mobile-model-x.jpeg')",
+        'solar-panels': "url('./images/solar-panels.jpeg')",
+        'mobile-solar-panels': "url('./images/mobile-solar-panels.jpeg')",
+        'solar-roof': "url('./images/solar-roof.jpeg')",
+        'mobile-solar-roof': "url('./images/mobile-solar-roof.jpeg')",
+        accessories: "url('./images/accessories.jpeg')",
+        'mobile-accessories': "url('./images/mobile-accessories.jpeg')",
       }),
       fontFamily: {
         ubuntu: ['Ubuntu'],
